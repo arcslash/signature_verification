fcfe7239afd63636de727063383633a82d9cff01
Ishara Abeykoon <isharaab@masholdings.com>
2019-08-25 21:59:12 +0530

    modifying code to use simase network, adding CNNS

--- core/core_api.py
+++ core/core_api.py
@@ -1,3 +1,72 @@
 import core.model
+from sklearn.model_selection import train_test_split
 # Maximum signature size
+import os
+import numpy as np
+from PIL import Image
 canvas_size = (952, 1360)
+
+
+def load_dataset(path='data/dataset/'):
+    #loading dataset to X
+    X = {}
+    Y = {}
+    original_img_path = os.path.join(path, 'genuine/')
+    forged_img_path = os.path.join(path, 'forged/')
+    original_imgs = os.listdir(original_img_path)
+    forged_imgs = os.listdir(forged_img_path)
+
+    #load data into memory
+    current_iter = 1
+    for original in original_imgs:
+        print(original)
+        image = Image.open(os.path.join(original_img_path, original))
+        image = np.asarray(image)
+        print("Image shape:", image.shape)
+        if not original.split("_")[1] in X.keys():
+            Y[original.split("_")[1]] = []
+            X[original.split("_")[1]] = []
+            X[original.split("_")[1]].append(image)
+            Y[original.split("_")[1]].append(1)
+        else:
+            X[original.split("_")[1]].append(image)
+            Y[original.split("_")[1]].append(1)
+
+    for forge in forged_imgs:
+        print(forge)
+        image = Image.open(os.path.join(forged_img_path, forge))
+        image = np.asarray(image)
+        print("Image shape:", image.shape)
+        if not original.split("_")[1] in X.keys():
+            Y[forge.split("_")[1]] = []
+            X[forge.split("_")[1]] = []
+            X[forge.split("_")[1]].append(image)
+            Y[forge.split("_")[1]].append(0)
+        else:
+            X[forge.split("_")[1]].append(image)
+            Y[forge.split("_")[1]].append(0)
+
+
+
+
+    X = list(X.values())
+    X = np.array(X)
+    print("shape:", X.shape)
+    Y = list(Y.values())
+    Y = np.array(Y)
+    print("shape:", Y.shape)
+
+    return X, Y
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    load_dataset("../data/dataset/")
+
+
+
--- core/model.py
+++ core/model.py
@@ -1,26 +1,48 @@
 import tensorflow as tf
 
 
-def build_model(input_shape):
-    model = tf.keras.Sequential([
-        tf.keras.layers.Conv2D(filters=96, kernel_size=11, strides=4, input_shape=input_shape),
-        tf.keras.layers.BatchNormalization(),
-        tf.keras.layers.MaxPool2D(pool_size=3, strides=2),
 
-        tf.keras.layers.Conv2D(filters=256, kernal=5, padding=2),
-        tf.keras.layers.BatchNormalization(),
-        tf.keras.layers.MaxPool2D(pool_size=3, strides=2),
+def build_model(input_shape = (952, 1360)):
+    """
+        Model architecture
+    """
 
-        tf.keras.layers.Conv2D(filters=384, kernel_size=3, padding=1),
-        tf.keras.layers.BatchNormalization(),
-        tf.keras.layers.Conv2D(filters=384, kernel_size=3, padding=1),
-        tf.keras.layers.BatchNormalization(),
-        tf.keras.layers.Conv2D(filters=256, kernel_size=3, padding=1),
-        tf.keras.layers.BatchNormalization(),
+    # Define the tensors for the two input images
+    left_input = tf.keras.layers.Input(input_shape)
+    right_input = tf.keras.layers.Input(input_shape)
 
-        tf.keras.layers.Dense(units=2048),
-        tf.keras.layers.Dense(units=2048)
+    # Convolutional Neural Network
+    model = tf.layers.Sequential([
+        tf.keras.layers.Conv2D(64, (10, 10), activation = 'relu', input_shape = input_shape, kernel_initializer = 'random_uniform', kernel_regularizer = tf.keras.regularizers.l2(2e-4)),
+        tf.keras.layers.MaxPool2D(),
+        tf.keras.layers.Conv2D(128, (7, 7), activation='relu', kernel_initializer = 'random_uniform', bias_initializer = 'zeros', kernel_regularizer=tf.keras.regularizers.l2(2e-4)),
+        tf.keras.layers.MaxPool2D(),
+        tf.keras.layers.Conv2D(128, (4, 4), activation='relu', kernel_initializer='random_uniform', bias_initializer='zeros', kernel_regularizer=tf.keras.regularizers.l2(2e-4)),
+        tf.keras.layers.MaxPooling2D(),
+        tf.keras.layers.Conv2D(256, (4, 4), activation='relu', kernel_initializer='random_uniform', bias_initializer='zeros', kernel_regularizer=tf.keras.regularizers.l2(2e-4)),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(4096, activation='sigmoid',kernel_regularizer=l2(1e-3), kernel_initializer='random_uniform', bias_initializer='zeros')
 
     ])
 
-    return model
+
+    # Generate the encodings (feature vectors) for the two images
+    encoded_l = model(left_input)
+    encoded_r = model(right_input)
+
+    # Add a customized layer to compute the absolute difference between the encodings
+    L1_layer = tf.keras.Lambda(lambda tensors: tf.keras.abs(tensors[0] - tensors[1]))
+    L1_distance = L1_layer([encoded_l, encoded_r])
+
+    # Add a dense layer with a sigmoid unit to generate the similarity score
+    prediction = tf.keras.layers.Dense(1, activation='sigmoid', bias_initializer='zeros')(L1_distance)
+
+    # Connect the inputs with the outputs
+    siamese_net = tf.keras.Model(inputs=[left_input, right_input], outputs=prediction)
+
+
+
+    return siamese_net
+
+if __name__ == '__main__':
+    build_model()
